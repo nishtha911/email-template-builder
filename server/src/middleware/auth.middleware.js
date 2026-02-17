23815ba415c2ec30cdbc4c626dbfaf62b72a52d6
Nishtha Pardeshi <nishthapardesi2005@gmail.com>
2026-02-17 11:50:41 +0530

    added the 'me' controller

--- server/src/middleware/auth.middleware.js
+++ server/src/middleware/auth.middleware.js
@@ -1,7 +1,6 @@
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-  // 1. Get token from the Header
   const authHeader = req.headers.authorization;
   
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
@@ -11,10 +10,8 @@ const authMiddleware = (req, res, next) => {
   const token = authHeader.split(" ")[1];
 
   try {
-    // 2. Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET || "secret");
     
-    // 3. Add user ID to the request object
     req.userId = decoded.userId;
     next();
   } catch (err) {
